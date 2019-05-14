0b2de6c6fa6aaa5cf877640d8d2631f6a66e0308
Spinzed <davor.najev@gmail.com>
2019-05-14 18:05:38 +0200

    Cars are now 2 pixels long and are faster

--- microbit.py
+++ microbit.py
@@ -2,6 +2,14 @@
 from microbit import *
 import random
 
+def try_set_pixel(x, y, b):
+    if x in range(0, 5) and y in range(0, 5):
+        display.set_pixel(x, y, b)
+
+def makeNewCar():
+    global car
+    car = Car()
+
 class Frog():
     def __init__(self, x):
         self.x = x 
@@ -25,37 +33,34 @@ class Car():
     def __init__(self):
         self.x = random.randint(0,4)
         self.y = 0
-        self.brightness = 5
+        self.parts = [[self.x, self.y], [self.x, self.y-1]]
+        self.brightness = 4
         self.isOnScreen = True
         display.set_pixel(self.x, self.y, self.brightness)
-         
     def move_down(self):
-        if self.y != 4:
-            display.set_pixel(self.x, self.y, 0)
-            display.set_pixel(self.x, self.y+1, self.brightness)
+        if self.y != 6:
             self.y += 1;
+            self.parts = [[self.x, self.y], [self.x, self.y-1]]
+            try_set_pixel(self.x, self.y-2, 0)
+            try_set_pixel(self.x, self.y, self.brightness)
         else:
             self.kill();
     def kill(self):
         if self.isOnScreen:
-            display.set_pixel(self.x, self.y, 0)
+            try_set_pixel(self.x, self.y-1, 0)
             self.isOnScreen = False
             makeNewCar()
-
-def makeNewCar():
-    global car
-    car = Car()
         
 frog = Frog(2)
 car = Car()
 counter = 0
 
-while True:
+while frog.isAlive:
     if button_a.was_pressed():
         frog.strafe("left")
     if button_b.was_pressed():
         frog.strafe("right")
-    if int(running_time()/1000) != counter:
+    if int(running_time()/600) != counter:
         counter += 1
         car.move_down()
         #display.scroll(str(int(running_time()/1000)))
