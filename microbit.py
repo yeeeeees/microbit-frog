7bac532d1b8f54affcf85a5468420a38e98cdc47
Spinzed <46136813+Spinzed@users.noreply.github.com>
2019-05-18 10:13:49 +0200

    Fixed a bug which caused memory leakage, the project is now fully object-oriented

--- microbit.py
+++ microbit.py
@@ -1,83 +1,89 @@
 from microbit import *
 import random
 
-def try_set_pixel(x, y, b):
-    if x in range(0, 5) and y in range(0, 5):
-        display.set_pixel(x, y, b)
-
-def makeNewCar():
-    global car
-    car = Car()
+class Game():
+    def __init__(self):
+        self.frog = Frog(self, 2)
+        self.cars = []
+        car = Car(self)
+        self.cars.append(car)
+        self.counter_spawn = 0
+        self.counter_move = 0
+        self.score = 0
+        self.isStarted = False
+    def start(self):
+        self.isStarted = True
+        while self.isStarted:
+            if button_a.was_pressed():
+                self.frog.strafe("left")
+            if button_b.was_pressed():
+                self.frog.strafe("right")
+            if int(running_time()/600) == self.counter_move:
+                self.counter_move += 1
+                for i in self.cars:
+                    i.move_down()
+            if int(running_time()/5000) == self.counter_spawn:
+                self.counter_spawn += 1
+                #display.scroll(len(cars))
+                if len(self.cars) < 20:
+                    car = Car(self)
+                    self.cars.append(car)
+            for i in self.cars:
+                for j in i.parts:
+                    if (self.frog.x, self.frog.y) == (j[0], j[1]):
+                        self.frog.die()
+                        self.isStarted = False
+    @staticmethod
+    def try_set_pixel(x, y, b):
+        if x in range(0, 5) and y in range(0, 5):
+            display.set_pixel(x, y, b)
 
 class Frog():
-    def __init__(self, x):
+    def __init__(self, game, x):
+        self.game = game
         self.x = x
         self.y = 4
         self.brightness = 7
         self.isAlive = True
-        display.set_pixel(self.x, self.y, self.brightness)
+        self.game.try_set_pixel(self.x, self.y, self.brightness)
     def strafe(self, pos):
-        display.set_pixel(self.x, self.y, 0)
+        self.game.try_set_pixel(self.x, self.y, 0)
         if self.x < 4 and pos == "right":
             self.x += 1
         if self.x > 0 and pos == "left":
             self.x -=1
-        display.set_pixel(self.x, self.y, self.brightness)  
+        self.game.try_set_pixel(self.x, self.y, self.brightness)  
     def die(self):
-        #global score
         self.isAlive = False
         display.scroll("u ded fam")
-        display.scroll("score : " + str(score))
+        display.scroll("score: " + str(self.game.score))
         display.clear()
         
 class Car():
-    def __init__(self):
-        global cars
+    def __init__(self, game):
+        self.game = game
         self.x = random.randint(0,4)
         self.y = 0
         self.parts = [[self.x, self.y], [self.x, self.y-1]]
         self.brightness = 4
         self.isOnScreen = True
-        display.set_pixel(self.x, self.y, self.brightness)
-        cars.append(self)
+        self.game.try_set_pixel(self.x, self.y, self.brightness)
     def move_down(self):
         if self.y != 6:
-            self.y += 1;
-            self.parts = [[self.x, self.y], [self.x, self.y-1]]
-            try_set_pixel(self.x, self.y-2, 0)
-            try_set_pixel(self.x, self.y, self.brightness)
+            self.update(self.y+1)
         else:
-            self.kill();
-    def kill(self):
+            self.reset()
+    def update(self, y):
+        self.y = y
+        self.parts = [[self.x, self.y], [self.x, self.y-1]]
+        self.game.try_set_pixel(self.x, self.y-2, 0)
+        self.game.try_set_pixel(self.x, self.y, self.brightness)
+    def reset(self):
         if self.isOnScreen:
-            global score
-            try_set_pixel(self.x, self.y-1, 0)
+            self.game.try_set_pixel(self.x, self.y-1, 0)
             self.isOnScreen = False
-            makeNewCar()
-            score += 1
-        
-frog = Frog(2)
-cars = []
-car = Car()
-counter_spawn = 0
-counter_move = 0
-score = 0
+            self.game.score += 1
+            self.update(self.y+1)
 
-while frog.isAlive:
-    if button_a.was_pressed():
-        frog.strafe("left")
-    if button_b.was_pressed():
-        frog.strafe("right")
-    if int(running_time()/600) == counter_move:
-        counter_move += 1
-        for i in cars:
-            i.move_down()
-    if int(running_time()/5000) == counter_spawn:
-        counter_spawn += 1
-        #display.scroll(len(cars))
-        if len(cars) < 20:
-            car = Car()
-    for i in cars:
-        for j in i.parts:
-            if (frog.x, frog.y) == (j[0], j[1]):
-                frog.die()
+game = Game()
+game.start()
